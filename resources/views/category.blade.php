9c0a232da974015e61c972cf89cc059fc1c795cf
Pedro Pereira <Pedro Pereira>
2016-11-06 03:50:29 -0200

    Faz a pagina de categoria

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,20 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
+<section class="conteudo">
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    <p>{{ $category->name }}</p>
-                    <p>{{ $category->description }}</p>
-                    <p>{{ $category->video }}</p>
-                    <br><a href="{{ url('question?category='.$category->id) }}">PERGUNTAS!</a>
+                <div id="painel">
+                    <div class="panel-heading">
+                        <h3>
+                            {{ $category->name }}
+                        </h3>
+                    </div>
+                    <div class="panel-body">
+                        <p>{{ $category->description }}</p>
+                        <p>{{ $category->video }}</p>
+                        <br><a href="{{ url('question?category='.$category->id) }}">PERGUNTAS!</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+</section>
 @endsection
